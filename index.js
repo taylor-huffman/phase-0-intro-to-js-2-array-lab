49555d6e4e4c26f12f0ff824596deafde42ca08f
Taylor Huffman <taylor@crucialartists.com>
2021-11-18 11:21:10 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const allCats = [...cats, "Broom"];
+    return allCats;
+}
+
+function prependCat(name) {
+    const allCats = ["Arnold", ...cats];
+    return allCats;
+}
+
+function removeLastCat() {
+    const newCatArray = cats.slice(0, -1);
+    return newCatArray;
+}
+
+function removeFirstCat() {
+    const newCatArray = cats.slice(1);
+    return newCatArray;
+}
